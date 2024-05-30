892eff2a3225c6be1d82e46a63fe02c5193cc4c7
1Dr490n <idragon3066@gmail.com>
2024-05-31 00:36:27 +0200

    fe

--- url.js
+++ url.js
@@ -1,5 +1,4 @@
-//const url = "ws://localhost:1989";
-const url = "wss://collie-known-wildly.ngrok-free.app";
+const url = location.href.includes("file") ? "ws://localhost:1989" : "wss://collie-known-wildly.ngrok-free.app";
 
 if (false) {
     window.alert("The server is offline right now. Please try again in a few minute.");
